5880d303def6694c1e41060d755921c995a1a639
Boxuan Zhang <capture1969@hotmail.com>
2024-02-17 21:32:28 +0800

    Push by autoPublisher: 2024/02/17,21:32:28

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ Put the "autoPublisher.bat" file in your git repo's root directory.
 Run it.
 # Feature
 1. It can create schtask to run itself.
-2. It very small --- Only about 3KB!
+2. It very small --- Only about 4KB!
 3. It can automate the git push process.
+3. It can save some of your configuration and some of your choices to the registry, location: HKCU\Software\autoPublisher
--- autoPublisher.bat
+++ autoPublisher.bat
@@ -4,7 +4,7 @@ set tempFile=%temp%\regTemp.txt
 set localhost=127.0.0.1
 set gitIP=github.com
 set schtaskName=autoPublisherTimer
-set ver=autoPublisher v2.4.4
+set ver=autoPublisher v2.4.5
 title %ver% running...
 echo Welcome to use %ver%
 :: Go to target direction
@@ -13,83 +13,84 @@ cd /d %~dp0
 echo .>%tempFile%
 reg query HKCU\Software\autoPublisher /v routerIP > %tempFile% 2>nul
 if %errorlevel%==0 (
-	for /f "tokens=3" %%i in (%tempFile%) do set routerIP=%%i
-	set noRIP=0
+    for /f "tokens=3" %%i in (%tempFile%) do set routerIP=%%i
+    set noRIP=0
 ) else (
-	set noRIP=1
+    set noRIP=1
 )
 reg query HKCU\Software\autoPublisher /v schTask > %tempFile% 2>nul
 if %errorlevel%==0 (
-	for /f "tokens=3" %%i in (%tempFile%) do set noTask=%%i
+    set noTask=1
 ) else (
-	set noTask=0
+    set noTask=0
 )
 :: Network status check
 echo Checking network status....
 :: Network local status check
 ping %localhost% -n 1 2>nul>nul
 if %errorlevel%==0 (
-	echo Current network status [Local]: Normal
+    echo Current network status [Local]: Normal
 ) else (
-	echo Current network status [Local]: Abnormal
-	echo Error: The computer does not have the network function enabled / %ver% does not have the ability to access the network
-	echo And the push process is refused
-	goto exit
+    echo Current network status [Local]: Abnormal
+    echo Error: The computer does not have the network function enabled / %ver% does not have the ability to access the network
+    echo And the push process is refused
+    goto exit
 )
 :: Network router status check
-if noRIP==0 (
-	choice /c YN /t 10 /d N /n /m "Current router IP address: %routerIP%, please confirm whether to change the router IP address, yes Y, no N"
-	if %errorlevel%==2 goto rcheck
-)
+if %noRIP%==1 goto lcheck
+echo Current router IP address: %routerIP%
+choice /c YN /t 10 /d N /n /m "Please confirm whether to change the router IP address, yes Y, no N"
+if %errorlevel%==2 goto rcheck
+:lcheck
 set /p routerIP=Please enter your router IP address (default: 192.168.0.1): 
 if not defined routerIP set routerIP=192.168.0.1
 :rcheck
 ping %routerIP% -n 1 2>nul>nul
 if %errorlevel%==0 (
-	echo Current network status [Router]: Normal
+    echo Current network status [Router]: Normal
 ) else (
-	echo Current network status [Router]: Abnormal
-	echo Error: %ver% can not access network and the push process is refused
-	goto exit
+    echo Current network status [Router]: Abnormal
+    echo Error: %ver% can not access network and the push process is refused
+    goto exit
 )
 :: Network github status check
 ping %gitIP% -n 1 2>nul>nul
 if %errorlevel%==0 (
-	echo Current network status [Github]: Normal
-	goto gsc
+    echo Current network status [Github]: Normal
+    goto gsc
 ) else (
-	echo Current network status [Github]: Abnormal
-	echo Error: %ver% can not access github and the push process is refused
-	goto exit
+    echo Current network status [Github]: Abnormal
+    echo Error: %ver% can not access github and the push process is refused
+    goto exit
 )
 :: Git status check
 :gsc
 git status 2>nul>nul
 if %errorlevel%==0 (
-	echo Current repository status: Normal
-	goto task
+    echo Current repository status: Normal
+    goto task
 ) else (
-	echo Current repository status: Abnormal
-	echo Error: The repository does not exist and the push process is refused
-	goto exit
+    echo Current repository status: Abnormal
+    echo Error: The repository does not exist and the push process is refused
+    goto exit
 )
 :: Schtask status check
 :task
 schtasks /query /tn %schtaskName% 2>nul>nul
 if %errorlevel%==0 (
-	echo Current schtask status: Normal
-	goto choose
+    echo Current schtask status: Normal
+    goto choose
 ) else (
-	echo Current schtask status: Abnormal
-	goto schtask
+    echo Current schtask status: Abnormal
+    goto schtask
 )
 :: Schtask config
 :schtask
-if noTask==1 goto choose
+if %noTask%==1 goto choose
 choice /c YN /t 10 /d Y /n /m "Please confirm whether to create scheduled task, yes Y, no N"
 if %errorlevel%==2 (
-	set noTask=1
-	goto choose
+    set noTask=1
+    goto choose
 )
 set /p t=Please enter your expected scheduled task execution time per day(24 hours) (default: 18): 
 if not defined t set t=18
@@ -107,7 +108,7 @@ echo Initialization complete, starting push process...
 title %ver% pushing...
 echo Adding all changes...
 git add .
-echo Commiting all changes...
+echo Committing all changes...
 git commit -m "Push by autoPublisher: %date:~0,10%,%time:~0,8%" 2>nul>nul
 echo Merging all remote changes to local...
 git pull 2>nul>nul 
@@ -120,8 +121,8 @@ reg add HKCU\Software\autoPublisher /v routerIP /t REG_SZ /d %routerIP% /f 2>nul
 reg add HKCU\Software\autoPublisher /v schTask /t REG_DWORD /d 1 /f 2>nul>nul
 :exit
 :: Program end
-title %ver% process has ened
+title %ver% process has ended
 echo Thank you for using %ver%
 echo Please press any key to continue...
 pause 2>nul>nul
-exit
+cls
